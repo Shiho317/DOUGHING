b34cbce8c098c5ac5a9bcf20b308bbe464692187
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-20 06:19:24 -0800

    added footer

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -1,8 +1,16 @@
+import './Footer.css';
 
 const Footer = () => {
   return(
     <div>
-
+      <div className="footer">
+        <p>&copy; 2022 Shiho-N.DEV</p>
+        <ul>
+          <li>Media</li>
+          <li>Privacy</li>
+          <li>Terms</li>
+        </ul>
+      </div>
     </div>
   )
 };
