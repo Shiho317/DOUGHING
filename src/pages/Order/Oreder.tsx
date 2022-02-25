c6472fca8840db5ccd20076a0492f2a4052e61c2
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-24 21:16:11 -0800

    added elements

--- src/pages/Order/Oreder.tsx
+++ src/pages/Order/Oreder.tsx
@@ -1,9 +1,26 @@
+import Header from '../Header/Header';
+import logoWhite from '../../images/doughing-logo-white.svg';
 import './Order.css';
+import Footer from '../Footer/Footer';
 
 const Order = () => {
   return(
     <div className="order-section">
-      <h1>Order</h1>
+      <Header logo={logoWhite}/>
+      <div className="order-details-wrapper">
+        <h1>Your pizza details</h1>
+        <h2>Total</h2>
+        <div className='paidBy'>
+          <p>paid by<br/></p>
+          <ul>
+            <li>Credit Card</li>
+            <li>Paypal</li>
+            <li>Apple Pay</li>
+            <li>Cash</li>
+          </ul>
+        </div>
+      </div>
+      <Footer/>
     </div>
   )
 };
